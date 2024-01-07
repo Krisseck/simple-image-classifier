3e54dda5708c2f7f549c8e32b04a2091cfda8cc2
Kristian Polso <kristian@polso.info>
2024-01-07 12:26:20 +0200

    Don't reuse the graph when loading files

--- app/label.py
+++ app/label.py
@@ -39,22 +39,25 @@ class Classify(object):
                                   input_width=299,
                                   input_mean=0,
                                   input_std=255):
-        input_name = "file_reader"
+        
+        with tf.Graph().as_default():
 
-        file_reader = tf.read_file(file_name, input_name)
+            input_name = "file_reader"
+
+            file_reader = tf.read_file(file_name, input_name)
 
         
-        image_reader = tf.image.decode_jpeg(
-            file_reader, channels=3, name="jpeg_reader")
+            image_reader = tf.image.decode_jpeg(
+                file_reader, channels=3, name="jpeg_reader")
 
-        float_caster = tf.cast(image_reader, tf.float32)
-        dims_expander = tf.expand_dims(float_caster, 0)
-        resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-        normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-        sess = tf.Session()
-        result = sess.run(normalized)
+            float_caster = tf.cast(image_reader, tf.float32)
+            dims_expander = tf.expand_dims(float_caster, 0)
+            resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+            normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+            sess = tf.Session()
+            result = sess.run(normalized)
 
-        return result
+            return result
 
     def load_labels(self, label_file):
         label = []
